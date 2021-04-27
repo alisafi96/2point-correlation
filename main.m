07df3e3a26043755af1a1eb2b5b872db81c76d18
alisafi96 <alisafi84@live.de>
2021-04-27 14:10:57 +0200

    Simpler EBSD

--- main.m
+++ main.m
@@ -1,38 +1,36 @@
-%% Import Script for EBSD Data
-%
-% This script was automatically created by the import wizard. You should
-% run the whoole script or parts of it in order to import your data. There
-% is no problem in making any changes to this script.
-
 %% Specify Crystal and Specimen Symmetries
 
 % crystal symmetry
-CS = {... 
-  'notIndexed',...
-  crystalSymmetry('321', [4.9 4.9 5.4], 'X||a', 'Y||b*', 'Z||c*', 'mineral', 'Quartz', 'color', [0.53 0.81 0.98])};
+CS = crystalSymmetry('m-3m', [3.7 3.7 3.7], 'mineral', 'Iron');
 
 % plotting convention
-% setMTEXpref('xAxisDirection','west');
+% setMTEXpref('xAxisDirection','east');
 % setMTEXpref('zAxisDirection','outOfPlane');
+
 plotx2east;
 plotzOutOfPlane;
 
 %% Specify File Names
 
 % path to files
-pname = '/Users/ali/Documents/GitHub/2point-correlation/data';
+pname = '\\juno\Homes\user\asafi\2point-correlation\data';
 
 % which files to be imported
-fname = [pname '/test.txt'];
+fname = [pname '\LMD_EBSD.txt'];
 
 %% Import the Data
 
 % create an EBSD variable containing the data
-% ebsd = EBSD.load(fname,CS,'interface','ang',...
-%  'convertEuler2SpatialReferenceFrame');
+ebsd = EBSD.load(fname,CS,'interface','generic',...
+  'ColumnNames', { 'phi1' 'Phi' 'phi2' 'x' 'y'}, 'Bunge');
 
-ebsd = EBSD.load(fname,'cs',CS,'ColumnNames',{'x','y','Euler1','Euler2','Euler3','phase'});
+%%
+plot(ebsd,ebsd.orientations)
 
 %% Calculate WignerD/GSH functions
 
-D = WignerD(ebsd('Quartz').orientations);
+% bandwidth or degree
+l = 1;
+% D = WignerD(ebsd.orientations(1),'degree',l);
+% D = WignerD(ebsd.orientations(1),'degree',l);
+D = WignerD(ebsd.orientations(1));
