f495803c41c25145b0557bbe0fcf3bc5f9403bf4
alisafi96 <alisafi84@live.de>
2021-06-15 19:05:51 +0200

    odf and mdf construction from WignerD

--- main.m
+++ main.m
@@ -1,21 +1,98 @@
 %% Import data
 mtexdata titanium;
 
-%% Calculate WignerD/GSH functions
-
-%odf_kernel = calcKernelODF(ebsd.orientations,'halfwidth',10*degree);
-
-%odf_psi = calcDensity(ebsd.orientations,'kernel',psi);
-
 %% Building the source Wigner D matrix to construct Fourier coefficients
 
-ori = ebsd(1:100).orientations;
-bandwidth = 20;
+% Define set of orientations & bandwidth
+ori = ebsd.orientations;
+bandwidth = 15;
 
 C = sum(conj(WignerD(ori, 'bandwidth', bandwidth)),2);
 C = C/length(ori);
 
-%% Evaluation of ODF (equation 4.21) 
+%% Calculate and compare odfs
+
+%odf_kernel = calcKernelODF(ebsd.orientations,'halfwidth',10*degree);
+
+% Harmoic expansion of the unimodal odf
+odf = calcDensity(ori,'Fourier','bandwidth', bandwidth, 'exact');
+
+% Direct calculation
+odf_C = FourierODF(C, ori.CS);
+
+
+%% Evaluate Fourier coefficients
+figure
+plotFourier(odf, 'bandwidth', bandwidth)
+
+figure
+plotFourier(odf_C, 'bandwidth', bandwidth)
+
+
+
+%% Evaluating odfs using pole figure plots
+
+h = { ...
+  Miller(0,0,0,2,ori.CS),...
+  Miller(1,0,-1,0,ori.CS),...
+  Miller(1,0,-1,1,ori.CS),...
+  Miller(1,0,-1,2,ori.CS),...
+  };
+
+figure
+plotPDF(odf,h,'antipodal');
+
+mtexColorbar
+%CLim(gcm,[0.0,2])
+
+figure
+plotPDF(odf_C,h,'antipodal');
+
+mtexColorbar
+%CLim(gcm,[0.0,2])
+
+
+%% Calculate Misorientations
+
+% Uncorrelated mdfs
+mdf = calcMDF(odf, 'bandwidth', bandwidth);
+mdf_C = calcMDF(odf_C);
+
+
+figure
+plotAxisDistribution(mdf)
+figure
+plotAxisDistribution(mdf_C)
+%% Calculate correlated MDF
+
+%grains = calcGrains(ebsd,'angle',10*degree);
+
+% Find grain boundary misorientation
+mori = grains.boundary('Titanium (Alpha)','Titanium (Alpha)').misorientation;
+
+mdf_correlated  = calcMDF(mori,'bandwidth',bandwidth);
+
+C_mori = sum(conj(WignerD(mori, 'bandwidth', bandwidth)),2);
+C_mori = C_mori/length(mori);
+
+mdf_C_correlated = FourierODF(C_mori,mori.CS,mori.SS,'antipodal');
+
+%% Plot Axis Distribution
+
+figure
+plotAxisDistribution(mdf_correlated);
+
+figure
+plotAxisDistribution(mdf_C_correlated);
+
+
+%% Calculate Kernel
+
+% psi = calcKernel(ori);
+odf_kernel = calcKernelODF(ebsd.orientations,'halfwidth',5*degree);
+%% Evaluation of ODF for a single orientation (can be replaced by MTEX function)
+
+
 
 % Wigner D of orientation of interest
 % Evaluate for random orientation
